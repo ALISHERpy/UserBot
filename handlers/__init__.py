71af01d0339b67f094c96dcf54b7cbf6bbb01f24
Alisher Akhmadov <a.alisher@tadi.uz>
2025-06-08 22:18:38 +0500

    userbot

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -13,7 +13,7 @@ def setup_routers() -> Router:
     # Agar kerak bo'lsa, o'z filteringizni o'rnating
     start.router.message.filter(ChatTypeFilter(chat_types=[ChatType.PRIVATE]))
 
-    # router.include_routers(admin.router, start.router, help.router, login.router, error_handler.router)
-    router.include_routers(echo.router)
+    router.include_routers(admin.router, start.router, help.router, login.router, error_handler.router)
+    # router.include_routers(echo.router)
 
     return router
--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -5,6 +5,6 @@ router = Router()
 
 @router.message()
 async def start_user(message: types.Message):
-    await message.answer("Bot remontda,xavfsizlik kuchaytirilmoqda !!\n\n9-uyun kuni soat 10:00gacha \n\nqayta ishlaydi\nIltimos sabr qilamiz")
-    # await message.answer("Salom,bot vaqtingchalik to'xtatildi,sabab foydalanuvchi juda ham kopayb ketdi,botni saqlab turung,sizga xabar beramiz bot ishga tushsa !!")
+    await message.answer(message.text)
+    # await message.answer("Bot remontda,xavfsizlik kuchaytirilmoqda !!\n\n9-uyun kuni soat 10:00gacha \n\nqayta ishlaydi\nIltimos sabr qilamiz")
 
